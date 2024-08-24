f0562bc9d23f6bc83d667139f95e95b17a2e3c4a
Rensytimsy <rensytimsy03@gmail.com>
2024-08-24 18:48:42 +0300

    express beginner

---
+++ express_js/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const port = process.env.PORT | 4000;
+
+const app = express();
+app.get("/", (req, res) => {
+    res.send("Hello world");
+});
+
+
+app.listen(port, () => console.log(`Server running at --> http://localhost:${port}`));
